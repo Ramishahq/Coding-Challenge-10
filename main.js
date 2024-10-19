4aee740060e23e2b58def93a6d5463d68b460d53
ramishahq <hoque2@usf.edu>
2024-10-19 15:20:12 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+//Task 2 
 const priceElement = document.getElementById('product-price');
 const sizeSelector = document.getElementById('size-selector');
 
 sizeSelector.addEventListener('change', function(event) {
     const selectedPrice = event.target.value;
     priceElement.textContent = `$${selectedPrice}`;
+});
+
+
+// Task 3 
+
+const stockStatusElement = document.getElementById('stock-status');
+const purchaseButton = document.getElementById('purchase-button');
+
+const stock = {
+    small: { price: 20, available: 5 },  
+    medium: { price: 22, available: 0 }, 
+    large: { price: 24, available: 10 }  
+}
+sizeSelector.addEventListener('change', function(event) {
+    const selectedSize = event.target.value;  
+    const selectedStock = stock[selectedSize];  
+    priceElement.textContent = `$${selectedStock.price}`;
+
+    // Update stock status and disable/enable purchase button
+    if (selectedStock.available) {
+        stockStatusElement.textContent = "In Stock";
+        purchaseButton.disabled = false;  
+    } else {
+        stockStatusElement.textContent = "Out of Stock";
+        purchaseButton.disabled = true;  
+    }
 });
